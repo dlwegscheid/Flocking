ac024eb0d0007689ee4d9493881a8fda575dba45
Ryan Bruns <ryan.bruns7@gmail.com>
2015-02-22 13:47:13 -0500

    Minor change to final, removed top bar from SelectionActivity.

--- app/src/main/java/edu/msu/wegschei/flocking/SelectionActivity.java
+++ app/src/main/java/edu/msu/wegschei/flocking/SelectionActivity.java
@@ -41,28 +41,6 @@ public class SelectionActivity extends ActionBarActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_selection, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     public void imageClicked(View view){
         //Convert the view to an ImageView
         ImageView imageView = (ImageView)view;
